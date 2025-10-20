5c7936563766538010a5e0a58424404eeda5456d
Argus Kapnoyal <ArgusK666@gmail.com>
2025-10-20 19:51:03 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-4355432604-bd63c",
+  "appId": "1:779521513083:web:3fe390424bce024108b4de",
+  "apiKey": "AIzaSyADIu0vvu0TH-jcsxiQZhjLGn8TLKTeTmo",
+  "authDomain": "studio-4355432604-bd63c.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "779521513083"
+};
